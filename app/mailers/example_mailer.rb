bd8537487042bc4ace908f8c6410207f448ba977
ndongA <boydbugle@gmai.com>
2017-06-03 08:28:35 +0300

    set up dotenv gem and variables .env

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,2 +1,7 @@
 class ExampleMailer < ApplicationMailer
+    default from: "myapp@example.com"
+
+    def sample_email(user)
+        @user = user
+        mail(to: @user.email, subject: "concerning payments")
 end
